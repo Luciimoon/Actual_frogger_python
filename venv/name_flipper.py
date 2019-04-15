2c2469ba7881134cc60b402e89c0382205ccfaee
compsci <compsci@comsci>
2019-04-15 10:21:36 -0600

    test

--- venv/name_flipper.py
+++
@@ -1,5 +0,0 @@
-s = 'smith, bob;jones, bill;doe, john'
-for lname, fname, in [q.split(",") for q in s.split(";")]:
-    # The split fuction defins fname and fname into a varible by slipting the data.
-    print(fname, lname)
-    # print the varibles fname lname 
