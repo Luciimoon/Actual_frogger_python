60a765ff0541bccd3cb2c75be87c960528b998e2
mitch <32491685+mitch-wallace@users.noreply.github.com>
2019-04-29 10:32:37 -0600

    Add files via upload

---
+++ venv/comstume/Anvil.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.locals import *
+import random
+
+
+class Anvil:
+    # set all class variables in the constructor
+    def __init__(self, newX, newY):
+        self.x = 50
+        self.y = 550
+        self.img = pygame.image.load("Anvil(1).gif")
+
+
+    # draw your image
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    # This method will be completed in the Running Man lab
+    # set x and y to newX and newY
+    def setPos(self, newX, newY):
+        self.x = newX
+        self.y = newY
+
+        while (True):
+            self.x = random.randint(0, 750)
+
+        pass
+
+    # DO NOT CHANGE THIS
+    # This method returns a rectangle - (x, y, width, height) - that represents
+    # the object
+    def getRec(self):
+        myRec = self.img.get_rect()
+        return (self.x, self.y, myRec[2], myRec[3])
+
+
